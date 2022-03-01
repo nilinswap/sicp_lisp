51f9d240f5df04d50295b76c99af915dfbb2a4c0
Swapnil Sharma <nilinswap@gmail.com>
2022-03-01 22:27:10 +0530

    resubmitted previous one

--- 2/2-41.scm
+++ 2/2-41.scm
@@ -35,3 +35,61 @@
 (triplet-sum 10 10)
 
 ;find all i,j,k less than equal n and filter then for their sum being equal to s
+(define (remove x l)
+    (filter 
+        (lambda (y)
+            (not (= y x))
+        ) l
+    )
+)
+
+(define (permutation l)
+    (if (null? l) (list nil)
+        (flatmap
+            (lambda (x)
+                (map 
+                    (lambda (rest) 
+                        (append (list x) rest)
+                    )
+                    (permutation (remove x l))
+                )
+            )
+            l
+        )
+    )
+)
+
+(permutation (enumerate 1 3))
+
+
+
+(define (all-distinct-triplets n)
+    (flatmap 
+        (lambda (x)
+            ( map (lambda(y-pairs) (append (list x) y-pairs))
+                (list-of-unique-pairs-without-x x)
+            )
+        )
+        (enumerate 1 n)
+    )
+)
+
+
+(define (list-of-unique-pairs-without-x x)
+    (flatmap 
+        (lambda (y)
+            (map 
+                (lambda (z) 
+                    (list y z)
+                )
+                (enumerate 1 (- y 1))
+            )
+        )
+        (enumerate 1 (- x 1))
+    )
+)
+
+(all-distinct-triplets 4)
+
+
+(triplet-sum 10 10)
